a27fcbf4bea3e81645c42f8ebca9464986c173b9
John Blox <johnbloxam2@gmail.com>
2021-12-16 15:39:36 +0000

    3rd commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 let title = document.getElementById("title")
 let newTitle = document.getElementById("newTitle")
+let newerTitle = document.getElementById("newerTitle")
 
 let changeTitle = () => title.style.color="blue"
 
-let changeNew = () => newTitle.innerHTML = "Old Title"
+let changeNew = () => newTitle.innerHTML = newerTitle.innerHTML
 
 console.log("Testing");
